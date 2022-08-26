d5a37666623e1f90f46e092cbf30e14c70413651
suloshiev <suloshiev@icloud.com>
2022-08-26 19:04:08 +0600

    change

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Like, Favourites
+from django.db.models import Avg
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -12,6 +13,14 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['raiting'] = instance.reviews.aggregate(Avg('raiting'))['raiting__avg']
+        repr['reviews'] = instance.reviews.count()
+        return repr
+
 
     def is_liked(self, product):
         user = self.context.get('request').user
--- product/views.py
+++ product/views.py
@@ -1,9 +1,10 @@
-from rest_framework import permissions
+from rest_framework import permissions, response
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import SearchFilter
+from raiting.serializers import ReviewSerializer
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -30,6 +31,19 @@ class ProductViewSet(ModelViewSet):
         if self.action == 'list':
             return serializers.ProductListSerializer
         return serializers.ProductDetailSerializer
+
+    @action(['GET', 'POST'], detail=True)
+    def reviews(self, request, pk=None):
+        product = self.get_object()
+        if request.method == 'GET':
+            reviews = product.reviews.all()
+            serializer = ReviewSerializer(reviews, many=True).data
+            return response.Response(serializer, status=200)
+        data = request.data
+        serializer = ReviewSerializer(data=data, context={'request':request, 'product':product})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=201)
 
     def get_permissions(self):
         # Лайкать , добавлять в избранное может аутентифицированный юзер
